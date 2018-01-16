b4e816043518f29f7578ff4c3a35da8b17ec6e7e
Fábio Henrique <fabiosjc@gmail.com>
2018-01-15 23:41:10 -0200

    Ammend

--- src/test/java/com/github/fabiosjc/JustGifItApplicationTests.java
+++ src/test/java/com/github/fabiosjc/JustGifItApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.fabiosjc.justgitit;
+package com.github.fabiosjc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JustGitItApplicationTests {
+public class JustGifItApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
